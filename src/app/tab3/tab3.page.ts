c0052458411c175a7bfea24901ca7e597f2e2800
Viraj Dhumal <mevirajdhumal@gmail.com>
2023-05-16 22:26:13 +0530

    Added Ratings in feedback

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,14 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent],
+  imports: [IonicModule, ExploreContainerComponent, CommonModule],
 })
-export class Tab3Page {
-  constructor() {}
+export class Tab3Page implements OnInit {
+
+  stars: number[] = [1, 2, 3, 4, 5];
+
+  selectedValue: number = 0;
+  isMouseover = true;
+
+  public data: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+  countStar(star: number) {
+    this.isMouseover = false;
+    this.selectedValue = star;
+    this.data = this.selectedValue;
+    console.log(this.data);
+  }
+
+  addClass(star: number) {
+    if (this.isMouseover) {
+      this.selectedValue = star;
+      console.log(this.selectedValue);
+
+    }
+  }
+
+  removeClass() {
+    if (this.isMouseover) {
+      this.selectedValue = 0;
+      console.log(this.selectedValue);
+    }
+  }
 }
